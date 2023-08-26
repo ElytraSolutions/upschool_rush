8ea116de1cd2d019713ef533a9986b2ae822887a
Suven-p <pandeysuven@gmail.com>
2023-08-26 18:10:01 +0545

    Enable web guard for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,6 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+
 require __DIR__.'/auth.php';
+require __DIR__.'/api.php';
