d56147afa61661a4eed7e3e07b3ca74b9144fc16
Suven-p <pandeysuven@gmail.com>
2023-08-27 11:58:53 +0545

    Run migrate on webhook run

--- routes/api.php
+++ routes/api.php
@@ -83,7 +83,7 @@ Route::post('/githubwebhook', function(Request $request) {
     $secret = env('GITHUB_WEBHOOK_SECRET');
     $hash = "sha1=" . hash_hmac('sha1', $request->getContent(), $secret);
     if ($request->ref != 'refs/heads/prod') {
-        Log::error('Invalid ref' . $request->all());
+        Log::error('Invalid ref');
         return 'Invalid ref';
     }
     if (strcmp($hash, $request->header('X-Hub-Signature')) == 0) {
@@ -91,9 +91,18 @@ Route::post('/githubwebhook', function(Request $request) {
         $process = new \Symfony\Component\Process\Process(['git', 'pull'], $root_path);
         $process->run();
         if(!$process->isSuccessful()) {
-            Log::info("Response from stdout: " . $process->getOutput());
-            Log::info("Response from stderr: " . $process->getErrorOutput());
-            Log::info("Response from status: " . $process->getExitCode());
+            Log::error("Response from stdout: " . $process->getOutput());
+            Log::error("Response from stderr: " . $process->getErrorOutput());
+            Log::error("Response from status: " . $process->getExitCode());
+            return 'Pull failed';
+        }
+        $process = new \Symfony\Component\Process\Process(['php', 'artisan', 'migrate'], $root_path);
+        $process->run();
+        if(!$process->isSuccessful()) {
+            Log::error("Response from stdout: " . $process->getOutput());
+            Log::error("Response from stderr: " . $process->getErrorOutput());
+            Log::error("Response from status: " . $process->getExitCode());
+            return 'Migrate failed';
         }
         return 'Success';
     } else {
