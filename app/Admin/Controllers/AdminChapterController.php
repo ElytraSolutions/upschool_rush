8843cc64fb8e4ebceb04dfa99770b9c40708adfa
Suven-p <pandeysuven@gmail.com>
2023-08-28 07:01:37 +0545

    Add chapter controller

---
+++ app/Admin/Controllers/AdminChapterController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Chapter;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class AdminChapterController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Chapter';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Chapter());
+
+        $grid->column('id', __('Id'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+        $grid->column('name', __('Name'));
+        $grid->column('slug', __('Slug'));
+        $grid->column('description', __('Description'));
+        $grid->column('course_id', __('Course id'));
+        $grid->column('active', __('Active'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Chapter::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+        $show->field('name', __('Name'));
+        $show->field('slug', __('Slug'));
+        $show->field('description', __('Description'));
+        $show->field('course_id', __('Course id'));
+        $show->field('active', __('Active'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Chapter());
+
+        $form->text('name', __('Name'));
+        $form->text('slug', __('Slug'));
+        $form->text('description', __('Description'));
+        $form->select('course_id', __('Course id'))->options(function ($id) {
+            $course = \App\Models\Course::find($id);
+
+            if ($course) {
+                return [$course->id => $course->name];
+            }
+        })->ajax('/admin/api/courses');
+        $form->switch('active', __('Active'))->default(1);
+
+        return $form;
+    }
+}
