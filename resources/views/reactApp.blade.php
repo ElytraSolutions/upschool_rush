0bd2dbe9c20aae7ba5c46511e3c9e18c62bcc7ad
GitHub Action <action@github.com>
2023-09-23 12:01:48 +0000

    deploy frontend

--- resources/views/reactApp.blade.php
+++ resources/views/reactApp.blade.php
@@ -1,50 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>upschool</title>
-    <script type="module" crossorigin src="/assets/index-c6d1476e.js"></script>
-    <link rel="modulepreload" crossorigin href="/assets/@babel-4ef42ede.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-18892649.js">
-    <link rel="modulepreload" crossorigin href="/assets/scheduler-765c72db.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-dom-81b76a28.js">
-    <link rel="modulepreload" crossorigin href="/assets/@remix-run-46b80ddf.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-router-cd24282a.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-router-dom-1a75177f.js">
-    <link rel="modulepreload" crossorigin href="/assets/axios-82afda87.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-is-a192e302.js">
-    <link rel="modulepreload" crossorigin href="/assets/clsx-1229b3e0.js">
-    <link rel="modulepreload" crossorigin href="/assets/hoist-non-react-statics-e96bed5f.js">
-    <link rel="modulepreload" crossorigin href="/assets/stylis-79144faa.js">
-    <link rel="modulepreload" crossorigin href="/assets/@emotion-3b974713.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-transition-group-b40fb521.js">
-    <link rel="modulepreload" crossorigin href="/assets/@mui-f5b003d9.js">
-    <link rel="modulepreload" crossorigin href="/assets/prop-types-9683e67b.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-easy-swipe-7c525ec3.js">
-    <link rel="modulepreload" crossorigin href="/assets/classnames-63c61219.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-responsive-carousel-c28b4bfe.js">
-    <link rel="modulepreload" crossorigin href="/assets/deepmerge-15f827de.js">
-    <link rel="modulepreload" crossorigin href="/assets/lodash-es-062508fc.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-fast-compare-23a9af43.js">
-    <link rel="modulepreload" crossorigin href="/assets/tiny-warning-c932d744.js">
-    <link rel="modulepreload" crossorigin href="/assets/formik-65be1323.js">
-    <link rel="modulepreload" crossorigin href="/assets/property-expr-38205fa5.js">
-    <link rel="modulepreload" crossorigin href="/assets/tiny-case-d0726479.js">
-    <link rel="modulepreload" crossorigin href="/assets/toposort-80543fdc.js">
-    <link rel="modulepreload" crossorigin href="/assets/yup-da92c934.js">
-    <link rel="modulepreload" crossorigin href="/assets/tslib-515cc0eb.js">
-    <link rel="modulepreload" crossorigin href="/assets/file-selector-bdf450a1.js">
-    <link rel="modulepreload" crossorigin href="/assets/attr-accept-6df8d728.js">
-    <link rel="modulepreload" crossorigin href="/assets/react-dropzone-e3fff22c.js">
-    <link rel="stylesheet" href="/assets/react-responsive-carousel-5440303d.css">
-    <link rel="stylesheet" href="/assets/index-ba48ac9c.css">
-    <link rel="stylesheet" href="/assets/font-awesome-3db6547a.css">
-  </head>
-  <body>
-    <div id="root"></div>
-    
-  </body>
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <title>upschool</title>
+  <script type="module" crossorigin src="/assets/index-dbae965e.js"></script>
+  <link rel="modulepreload" crossorigin href="/assets/@babel-4ef42ede.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-18892649.js">
+  <link rel="modulepreload" crossorigin href="/assets/scheduler-765c72db.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-dom-81b76a28.js">
+  <link rel="modulepreload" crossorigin href="/assets/@remix-run-46b80ddf.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-router-cd24282a.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-router-dom-1a75177f.js">
+  <link rel="modulepreload" crossorigin href="/assets/axios-82afda87.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-is-a192e302.js">
+  <link rel="modulepreload" crossorigin href="/assets/clsx-1229b3e0.js">
+  <link rel="modulepreload" crossorigin href="/assets/hoist-non-react-statics-e96bed5f.js">
+  <link rel="modulepreload" crossorigin href="/assets/stylis-79144faa.js">
+  <link rel="modulepreload" crossorigin href="/assets/@emotion-3b974713.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-transition-group-b40fb521.js">
+  <link rel="modulepreload" crossorigin href="/assets/@mui-10291432.js">
+  <link rel="modulepreload" crossorigin href="/assets/prop-types-9683e67b.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-easy-swipe-7c525ec3.js">
+  <link rel="modulepreload" crossorigin href="/assets/classnames-63c61219.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-responsive-carousel-c28b4bfe.js">
+  <link rel="modulepreload" crossorigin href="/assets/deepmerge-15f827de.js">
+  <link rel="modulepreload" crossorigin href="/assets/lodash-es-062508fc.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-fast-compare-23a9af43.js">
+  <link rel="modulepreload" crossorigin href="/assets/tiny-warning-c932d744.js">
+  <link rel="modulepreload" crossorigin href="/assets/formik-65be1323.js">
+  <link rel="modulepreload" crossorigin href="/assets/property-expr-38205fa5.js">
+  <link rel="modulepreload" crossorigin href="/assets/tiny-case-d0726479.js">
+  <link rel="modulepreload" crossorigin href="/assets/toposort-80543fdc.js">
+  <link rel="modulepreload" crossorigin href="/assets/yup-da92c934.js">
+  <link rel="modulepreload" crossorigin href="/assets/tslib-515cc0eb.js">
+  <link rel="modulepreload" crossorigin href="/assets/file-selector-bdf450a1.js">
+  <link rel="modulepreload" crossorigin href="/assets/attr-accept-6df8d728.js">
+  <link rel="modulepreload" crossorigin href="/assets/react-dropzone-e3fff22c.js">
+  <link rel="stylesheet" href="/assets/react-responsive-carousel-5440303d.css">
+  <link rel="stylesheet" href="/assets/index-5c66b6f3.css">
+  <link rel="stylesheet" href="/assets/font-awesome-3db6547a.css">
+</head>
+
+<body>
+  <div id="root"></div>
+  
+</body>
+
 </html>
