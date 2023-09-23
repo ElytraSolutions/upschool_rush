d0d340e1f087fa9db21f73d004211707c1ce2bb4
Suven-p <pandeysuven@gmail.com>
2023-09-23 13:17:24 +0545

    Add css to body for richcontent

--- resources/views/richContentView.blade.php
+++ resources/views/richContentView.blade.php
@@ -8,6 +8,13 @@
 
     <style>
         {{ $content->css }}
+        body {
+            display: flex !important;
+            justify-content: center !important;
+            flex-direction: column !important;
+            align-items: center !important;
+            width: 100vw;
+        }
     </style>
 </head>
 {!! $content->html !!}
