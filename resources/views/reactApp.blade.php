9e1277c25ad70ba94ed56a723938216e998a017f
GitHub Action <action@github.com>
2023-12-06 03:51:28 +0000

    deploy frontend

--- resources/views/reactApp.blade.php
+++ resources/views/reactApp.blade.php
@@ -8,7 +8,7 @@
 
 
   <title>Upschool</title>
-  <script type="module" crossorigin src="/assets/index-9d8b8196.js"></script>
+  <script type="module" crossorigin src="/assets/index-b9e8efe3.js"></script>
   <link rel="modulepreload" crossorigin href="/assets/@babel-4ef42ede.js">
   <link rel="modulepreload" crossorigin href="/assets/react-18892649.js">
   <link rel="modulepreload" crossorigin href="/assets/scheduler-765c72db.js">
@@ -56,7 +56,7 @@
   <link rel="stylesheet" href="/assets/react-responsive-carousel-5440303d.css">
   <link rel="stylesheet" href="/assets/react-toastify-93d8b085.css">
   <link rel="stylesheet" href="/assets/react-svg-map-cf9544dd.css">
-  <link rel="stylesheet" href="/assets/index-6f8f39c3.css">
+  <link rel="stylesheet" href="/assets/index-d68841f9.css">
   <link rel="stylesheet" href="/assets/font-awesome-3db6547a.css">
 </head>
 
